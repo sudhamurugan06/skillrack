9948686e3520675174b24058c3db019903dc9329
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2019-11-18 08:53:00 +0530

    Create powersof2.c

---
+++ powersof2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,c=0,r=1;
+    scanf("%d",&n);
+    for(int i=0 ;i<=n/2;i++)
+    {
+        r=2*r;
+        c++;
+        if(r==n)
+        {
+           for(int j=0;j<c-1;j++)
+           {
+               printf("2*");
+           }
+           break;
+        }
+
+    }
+    printf("2");
+
+}
