071cc156d5036965b1f2bff1b1e6109d55ebc276
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2019-11-21 20:18:34 +0530

    Create Allsamecharacters.c

---
+++ Allsamecharacters.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main
+{
+char s[1000];
+scanf("%s",s);
+int n=a[0],c=0,len=strlen(s);
+for(int i=0;i<=len;i++)
+{
+if(s[i]==n)
+{
+c++;
+}
+}
+if(c==len){printf("YES");}
+else{printf("NO");}
+}
