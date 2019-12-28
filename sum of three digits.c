c1e5ae12084b7c17cfabe37e894176d79e261801
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2019-12-28 10:21:41 +0530

    Update sum of three digits.c

--- sum of three digits.c
+++ sum of three digits.c
@@ -28,3 +28,54 @@ while(n!=0){
 }
 printf("%d",sum);
 }
+**************************or***********************************************************************************************
+int main()
+{
+    long int n;
+    int i=0,j,h=0,sum=0,k;
+    scanf("%ld",&n);
+    long int num=n;
+    while(n>0)
+    {
+        n=n/10;
+        i++;
+    }
+    if(i==3)
+    {
+        printf("%d",num);
+    }
+    else if(i==6)
+    {
+        int s[6];
+        for(j=0;j<6;j++)
+        {
+            s[j]=num%10;
+            num=num/10;
+        }
+        int a[3];
+        for(j=0;j<6;j=j+3)
+        {
+          a[h]=s[j]+s[j+1]*10+s[j+2]*100;
+          h++;
+
+        }
+        printf("%d",a[0]+a[1]);
+    }
+    else if(i==9)
+    {
+        int s[9];
+        for(j=0;j<9;j++)
+        {
+            s[j]=num%10;
+            num=num/10;
+        }
+        int a[3];
+        for(j=0;j<9;j=j+3)
+        {
+          a[h]=s[j]+s[j+1]*10+s[j+2]*100;
+          h++;
+
+        }
+        printf("%d",a[0]+a[1]+a[2]);
+    }
+}
