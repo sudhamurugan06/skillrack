a338379ecfe167ebb59faf398463360c4b237b85
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2020-01-10 21:17:45 +0530

    Update num with same digits.c

--- num with same digits.c
+++ num with same digits.c
@@ -28,3 +28,58 @@ int main()
     }
     if(v==0)printf("-1");
 }
+//***************************************************************or***************************************************************
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{  int n,a,c=1;
+   scanf("%d\n",&n);
+for(int i=0;i<n;i++)
+{ int f=0;
+  scanf("%d",&a);
+  int j=a;
+  while(j>=10)
+  { int r=j%10;
+    j/=10;
+    if(r!=j%10)
+     { f=1;
+       break;
+     }
+  }
+  if(f==0)
+  {printf("%d ",a);
+   c=0;
+  }
+}if(c) printf("-1");
+}
+//***********************************************************or************************************************************
+#include<stdio.h>
+#include <stdlib.h>
+int check(int a)
+{
+    int b;
+    b=a%10;
+    while(a>0)
+    {
+        if(b!=a%10)
+        return 0;
+        a/=10;
+    }
+    return 1;
+}
+int main()
+{
+    int i,j,k=0,l[1000],n,a;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a);
+        if(check(a))
+        {
+        printf("%d ",a);
+        k=1;
+        }
+    }
+    if(k==0)
+    printf("-1");
