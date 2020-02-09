6d6b6433456af169c3cc690f0135143786cd641a
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2020-02-09 10:25:35 +0530

    Create triangle wid_ pateern(_).c

---
+++ triangle wid_ pateern(_).c
@@ -0,0 +1,39 @@
+/***********if n=5...PATTERN AS BELOW:
+______                                                                                                                       
+\****/                                                                                                                       
+*\**/                                                                                                                        
+**\/ */
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,n,k,a=-1,b;
+    scanf("%d",&n);
+    k=n+2;
+    b=n+1;
+    for(i=0;a<k;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            if(i==0)
+            {
+                for(j=0;j<b;j++)
+                printf("_");
+                break;
+            }
+            else if(j==a)
+            printf("\\");
+            else if(j==k-1)
+            printf("/");
+            else
+            printf("*");
+        }
+        printf("\n");
+        k--;
+        a++;
+    }
+
+
+}
