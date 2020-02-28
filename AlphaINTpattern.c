45d5bde9df0df89856359bd8120527fddf93ee60
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2020-02-28 19:05:05 +0530

    Create AlphaINTpattern.c

---
+++ AlphaINTpattern.c
@@ -0,0 +1,14 @@
+ALPHABETS & INTEGERS ALTERNATE COLUMS: if N=7
+
+    1
+    2 a
+    3 b 1
+    4 c 2 a
+    5 d 3
+    6 e
+    7  
+********************************************************************************************
+
+
+      logic: if j column is even print alphabets and -2.
+             else print Nos and decrement by 2;
