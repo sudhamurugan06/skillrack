e0bc127b6cbf7eef020dd11579dde12e8c4bc184
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2020-02-29 18:55:48 +0530

    Update AlphaINTpattern.c

--- AlphaINTpattern.c
+++ AlphaINTpattern.c
@@ -12,3 +12,31 @@ ALPHABETS & INTEGERS ALTERNATE COLUMS: if N=7
 
       logic: if j column is even print alphabets and -2.
              else print Nos and decrement by 2;
+*******************************************************************************************
+for(int i=0;i<a;i++){
+    for(int j=0;j<a;j++){
+        if(j%2==0){
+            printf("%d ",i+1-j);
+        }
+        else{
+            char d;
+            d='a'+i-j;
+            if(d>122){
+                d=d-122+97;
+            }
+            printf("%c ",'a'+(i-j)%26);
+        }
+        if(i<=a/2){
+            if(i==j){
+                break;
+            }
+        }
+        else{
+            if(i+j==a-1){
+                break;
+            }
+        }
+    }
+    printf("\n");
+}
+}
